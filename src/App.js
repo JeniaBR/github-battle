7ea4cdbdd0cb69f48947fe627839f2acc08becbd
Jenia Brook <jeniabrook@gmail.com>
2017-04-14 20:28:38 +0300

    Add React Router Add Nav menu Refactor Home Component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import ReactRouter, {BrowserRouter, Route} from 'react-router-dom';
+import Nav from './components/Nav';
 import Popular from './components/Popular';
 import Main from './components/Main';
 import Home from './components/Home';
@@ -8,9 +10,13 @@ import './styles/index.css'
 class App extends Component {
   render(){
     return(
-      <div className='container'>
-        <Popular/>
-      </div>
+      <BrowserRouter> 
+        <div className='container'>
+          <Nav/>
+          <Route exact path='/' component={Home}/>
+          <Route path='/popular' component={Popular} />
+        </div>
+      </BrowserRouter>
     )
   }
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,17 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 import octacat from '../img/linktocat.png';
 import '../styles/styles.css'
 
 class Home extends Component {
     render(){
         return(
-            <div className="jumbotron col-sm-12 text-center">
+            <div className="home-container">
                 <h1>Github Battle</h1>
                 <p className="lead">Some fancy motto</p>
-                {/*<Link to='/playerOne'>
-                    <button type='button' className="btn btn-lg btn-success">Get Started</button>
-                </Link>*/}
+                <Link className='button' to='/battle'>
+                  Battle
+                </Link>
                 <img className="img-responsive imageCenter" src={octacat} alt="Octa Cat"/>
             </div>
         );
