2535b1c92f12a8634dd620e89321a5b4ae41ea8f
Jenia Brook <jeniabrook@gmail.com>
2017-04-21 21:25:20 +0300

    Remove some unnecessary files Some style change for responsive design

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -32,10 +32,8 @@ const RepoGrid = (props) => {
         return(
           <li key={repo.name} className="popular-item">
           <div className="popular-rank">#{index + 1}</div>
+          <img className="avatar" src={repo.owner.avatar_url} alt={`Avatar for ${repo.owner.login}`}/>
           <ul className="space-list-items">
-            <li>
-              <img className="avatar" src={repo.owner.avatar_url} alt={`Avatar for ${repo.owner.login}`}/>
-            </li>
             <li><a href={repo.html_url}>{repo.name}</a></li>
             <li>@{repo.owner.login}</li>
             <li>{repo.stargazers_count} stars</li>
--- src/components/Results.js
+++ src/components/Results.js
@@ -24,7 +24,7 @@ const Profile = (props) => {
 
 const Player = (props) => {
   return(
-    <div>
+    <div className='card'>
       <h1 className="header">{props.label}</h1>
       <h3 style={{textAlign: 'center'}}>Score: {props.score}</h3>
       <Profile info={props.profile}/>
